f0763f35c9478ea17100f12e9f2bb88d67efe68c
Jakub Kulik <krulicz@interia.pl>
2016-05-02 11:51:47 +0200

    RestClient final

--- src/main/java/wdsr/exercise3/client/ProductService.java
+++ src/main/java/wdsr/exercise3/client/ProductService.java
@@ -6,7 +6,14 @@ import java.util.Set;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.sun.glass.ui.Application;
 import wdsr.exercise3.model.Product;
 import wdsr.exercise3.model.ProductType;
 
@@ -14,64 +21,102 @@ public class ProductService extends RestClientBase {
 	protected ProductService(final String serverHost, final int serverPort, final Client client) {
 		super(serverHost, serverPort, client);
 	}
-	
+
+	@Context
+	Application application;
+	private final WebTarget statusTarget = baseTarget.path("/products");
+
 	/**
 	 * Looks up all products of given types known to the server.
-	 * @param types Set of types to be looked up
+	 * 
+	 * @param types
+	 *            Set of types to be looked up
 	 * @return A list of found products - possibly empty, never null.
 	 */
 	public List<Product> retrieveProducts(Set<ProductType> types) {
-		// TODO
-		return null;
+		return statusTarget.queryParam("type", types.toArray()).request().get(new GenericType<List<Product>>() {
+		});
 	}
-	
+
 	/**
 	 * Looks up all products known to the server.
+	 * 
 	 * @return A list of all products - possibly empty, never null.
 	 */
 	public List<Product> retrieveAllProducts() {
-		// TODO
-		return null;
-	}
-	
-	/**
-	 * Looks up the product for given ID on the server.
-	 * @param id Product ID assigned by the server
-	 * @return Product if found
-	 * @throws NotFoundException if no product found for the given ID.
-	 */
-	public Product retrieveProduct(int id) {
-		// TODO
-		return null;
-	}	
-	
-	/**
-	 * Creates a new product on the server.
-	 * @param product Product to be created. Must have null ID field.
-	 * @return ID of the new product.
-	 * @throws WebApplicationException if request to the server failed
-	 */
-	public int storeNewProduct(Product product) {
-		// TODO
-		return 0;
-	}
-	
-	/**
-	 * Updates the given product.
-	 * @param product Product with updated values. Its ID must identify an existing resource.
-	 * @throws NotFoundException if no product found for the given ID.
-	 */
-	public void updateProduct(Product product) {
-		// TODO
+		return statusTarget.request().get(new GenericType<List<Product>>() {
+		});
 	}
 
-	
+	/**
+	 * Looks up the product for given ID on the server.
+	 * 
+	 * @param id
+	 *            Product ID assigned by the server
+	 * @return Product if found
+	 * @throws NotFoundException
+	 *             if no product found for the given ID.
+	 */
+	public Product retrieveProduct(int id) {
+		return statusTarget.path(String.valueOf(id)).request().get(new GenericType<Product>() {
+		});
+	}
+
+	/**
+	 * Creates a new product on the server.
+	 * 
+	 * @param product
+	 *            Product to be created. Must have null ID field.
+	 * @return ID of the new product.
+	 * @throws WebApplicationException
+	 *             if request to the server failed
+	 */
+	public int storeNewProduct(Product product) {
+		Response response = statusTarget.request().post(Entity.entity(product, MediaType.APPLICATION_JSON),
+				Response.class);
+		if (response.getStatus() != Response.Status.CREATED.getStatusCode()) {
+			throw new WebApplicationException();
+		}
+		response.close();
+		return Integer.parseInt(
+				response.getLocation().toString().substring(response.getLocation().toString().lastIndexOf("/") + 1));
+	}
+
+	/**
+	 * Updates the given product.
+	 * 
+	 * @param product
+	 *            Product with updated values. Its ID must identify an existing
+	 *            resource.
+	 * @throws NotFoundException
+	 *             if no product found for the given ID.
+	 */
+	public void updateProduct(Product product) {
+		if (retrieveProduct(product.getId()) != null) {
+			statusTarget.path(String.valueOf(product.getId())).request()
+					.put(Entity.entity(product, MediaType.APPLICATION_JSON));
+			;
+		} else {
+			throw new NotFoundException();
+		}
+
+	}
+
 	/**
 	 * Deletes the given product.
-	 * @param product Product to be deleted. Its ID must identify an existing resource.
-	 * @throws NotFoundException if no product found for the given ID.
+	 * 
+	 * @param product
+	 *            Product to be deleted. Its ID must identify an existing
+	 *            resource.
+	 * @throws NotFoundException
+	 *             if no product found for the given ID.
 	 */
 	public void deleteProduct(Product product) {
-		// TODO
+		if (retrieveProduct(product.getId()) != null) {
+			statusTarget.path(String.valueOf(product.getId())).request().delete();
+		} else {
+			throw new NotFoundException();
+		}
+
 	}
 }
